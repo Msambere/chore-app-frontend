674cf3e35f1c0a6f5f308ecd5756bf2ee13650a9
Salma Anany <salmayousry5@gmail.com>
2025-01-28 11:46:15 -0800

    instailling MUI not working

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,9 +1,27 @@
-import {ReactElement} from "react";
 
-const SideBar = (): ReactElement<string> => {
+import {List, ListItem, ListItemButton, ListItemText} from '@mui/material';
+import { Link } from 'react-router';
+
+const Sidebar = () => {
+  const menuItems = [
+    {label: 'Chores', route: '/chores'},
+    {label: 'Login', route: '/login'},
+    {label: 'UserProfile', route: '/UserProfile'},
+    {label: 'Rewards', route: '/Rewards'},
+    {label: 'Mission', route: '/Mission'},
+
+  ]
   return (
-    <h1>This is were the Sidebar components will go</h1>
-  )
-}
+    <List>
+      {menuItems.map((item) => (
+        <ListItem key={item.label} component={(props) => <Link {...props} to={item.route!} />}>
+          <ListItemButton>
+            <ListItemText primary={item.label} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
-export default SideBar;
+export default Sidebar;
