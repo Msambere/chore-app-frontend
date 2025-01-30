0c3fb284fca668c69486019dd80a37c62476fca0
Amber <110812972+Msambere@users.noreply.github.com>
2025-01-30 16:00:48 -0600

    Renamed working mission form and deleted old one

--- src/Components/MissionPage/MissionForm/MissionForm.tsx
+++ src/Components/MissionPage/MissionForm/MissionForm.tsx
@@ -1,133 +1,192 @@
-// import MissionChoreResponse from "~/types/Response/MissionChoreResponse";
-// import { Dispatch, SetStateAction, useEffect, useState } from "react";
-// import { Box, Typography, MenuItem, Button } from "@mui/material";
-// import {Form, useSubmit} from "react-router";
-// import TextField from "@mui/material/TextField";
-// import UserData from "~/types/Response/UserData";
-// import { extractUserRecurrences } from "~/Helper Functions/extractUserRecurrences";
-// import { extractUserCategories } from "~/Helper Functions/extractUserCategories";
-// import { MissionRequest } from "~/types/Request/MissionRequest";
-// import { createNewMission } from "~/Helper Functions/ApiCalls";
-// import MissionResponse from "~/types/Response/MissionResponse";
-//
-// interface MissionProps {
-//   setMissionChores: Dispatch<SetStateAction<MissionChoreResponse[]>>;
-//   setStartMission: Dispatch<SetStateAction<boolean>>;
-//   userData: UserData;
-// }
-// const MissionForm = (props: MissionProps) => {
-//   const [recurrenceOptions, setRecurrenceOptions] = useState<string[]>();
-//   const [categoryOptions, setCategoryOptions] = useState<string[]>();
-//   const [missionRequestData, setMissionRequestData] = useState<MissionRequest>({
-//     recurrence: null,
-//     category: null,
-//     timeLimit: null,
-//   });
-//   useEffect(() => {
-//     setRecurrenceOptions(extractUserRecurrences(props.userData.chores));
-//     setCategoryOptions(extractUserCategories(props.userData.chores));
-//   }, []);
-//
-//   const handleInputChange = (
-//     fieldName: string,
-//     fieldValue: string | number,
-//   ): void => {
-//     console.log(fieldName, fieldValue);
-//     setMissionRequestData({ ...missionRequestData, [fieldName]: fieldValue });
-//   };
-//
-//   const handleStartMission = (event) => {
-//     event.preventDefault();
-//     console.log("MissionRequest being sent");
-//     // make api Post request
-//     createNewMission(props.userData.userId, missionRequestData).then(
-//       (response: MissionResponse) => {
-//         props.setMissionChores(response.missionChores);
-//         props.setStartMission(true);
-//         useSubmit();
-//       },
-//     );
-//   };
-//
-//   return (
-//     <>
-//       <Box sx={{ maxWidth: 400, margin: "auto", padding: 3 }}>
-//         <Typography variant="h4" gutterBottom>
-//           Create Mission
-//         </Typography>
-//         <Form method="post" onSubmit={(event) => handleStartMission(event)}>
-//           <TextField
-//             fullWidth
-//             select
-//             name="recurrence"
-//             label="Recurrence"
-//             margin="normal"
-//             required
-//             defaultValue={missionRequestData.recurrence || ""}
-//             onChange={(event) =>
-//               handleInputChange(event.target.name, event.target.value)
-//             }
-//           >
-//             {recurrenceOptions?.map((recurrence, index) => (
-//               <MenuItem key={index} value={recurrence}>
-//                 {recurrence}
-//               </MenuItem>
-//             ))}
-//           </TextField>
-//
-//           <TextField
-//             fullWidth
-//             name="category"
-//             label="Category"
-//             margin="normal"
-//             placeholder="Optional"
-//             defaultValue={missionRequestData.category || ""}
-//             onChange={(event) =>
-//               handleInputChange(event.target.name, event.target.value)
-//             }
-//           >
-//             {categoryOptions?.map((category, index) => (
-//               <MenuItem key={index} value={category}>
-//                 {category}
-//               </MenuItem>
-//             ))}
-//           </TextField>
-//
-//           <TextField
-//             fullWidth
-//             name="timeLimit"
-//             label="Time Limit (minutes)"
-//             type="number"
-//             margin="normal"
-//             required
-//             defaultValue=""
-//           />
-//
-//           <Button
-//             type="submit"
-//             variant="contained"
-//             color="primary"
-//             fullWidth
-//             sx={{ marginTop: 2 }}
-//             // onClick={(event) => handleStartMission(event)}
-//           >
-//             {/*{isSubmitting ? "Creating..." : "Create Mission"}*/}
-//             Start Mission!
-//           </Button>
-//         </Form>
-//
-//         {/* If you want a "Back" button */}
-//         <Button
-//           variant="text"
-//           color="secondary"
-//           onClick={() => window.history.back()}
-//           sx={{ marginTop: 2 }}
-//         >
-//           Back
-//         </Button>
-//       </Box>
-//     </>
-//   );
-// };
-//
-// export default MissionForm;
+import MissionChoreResponse from "~/types/Response/MissionChoreResponse";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import {
+  Box,
+  Typography,
+  MenuItem,
+  Button,
+  Alert,
+  AlertTitle,
+  Collapse,
+  IconButton,
+} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import UserData from "~/types/Response/UserData";
+import { extractUserRecurrences } from "~/Helper Functions/extractUserRecurrences";
+import { extractUserCategories } from "~/Helper Functions/extractUserCategories";
+import { MissionRequest } from "~/types/Request/MissionRequest";
+import { createNewMissionApiCall } from "~/Helper Functions/ApiCalls";
+import MissionResponse from "~/types/Response/MissionResponse";
+import CloseIcon from "@mui/icons-material/Close";
+
+interface MissionProps {
+  setMissionChores: Dispatch<SetStateAction<MissionChoreResponse[]>>;
+  setStartMission: Dispatch<SetStateAction<boolean>>;
+  userData: UserData;
+}
+
+const defaultMissionRequest: MissionRequest = {
+  recurrence: null,
+  category: null,
+  timeLimit: null,
+};
+
+const MissionForm = (props: MissionProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [recurrenceOptions, setRecurrenceOptions] = useState<string[]>();
+  const [categoryOptions, setCategoryOptions] = useState<string[]>();
+  const [missionRequestData, setMissionRequestData] = useState<MissionRequest>(
+    defaultMissionRequest,
+  );
+
+  useEffect(() => {
+    const recurrenceList: string[] = extractUserRecurrences(
+      props.userData.chores,
+    );
+    recurrenceList.push("Any");
+    setRecurrenceOptions(recurrenceList);
+    const categoryList: string[] = extractUserCategories(props.userData.chores);
+    categoryList.push("Any");
+    setCategoryOptions(categoryList);
+  }, []);
+
+  const handleInputChange = (
+    fieldName: string,
+    fieldValue: string | number | null,
+  ): void => {
+    if (fieldValue === "Any" || fieldValue === "Unlimited") {
+      fieldValue = null;
+    }
+    setMissionRequestData({ ...missionRequestData, [fieldName]: fieldValue });
+  };
+
+  const handleStartMission = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    createNewMissionApiCall(props.userData.userId, missionRequestData)
+      .then((response: MissionResponse) => {
+        props.setMissionChores(response.missionChores);
+        props.setStartMission(true);
+        setMissionRequestData(defaultMissionRequest);
+      })
+
+      .catch((error) => {
+        console.log(error.response.data.message);
+        setOpen(true);
+      })
+      .finally(() => {
+        console.log("Always print");
+      });
+  };
+
+  return (
+    <>
+      <Box sx={{ maxWidth: 400, margin: "auto", padding: 3 }}>
+        <Typography variant="h4" gutterBottom>
+          Create Mission
+        </Typography>
+        <form onSubmit={handleStartMission}>
+          <TextField
+            fullWidth
+            select
+            name="recurrence"
+            label="Recurrence"
+            margin="normal"
+            defaultValue="Any"
+            onChange={(event) =>
+              handleInputChange(event.target.name, event.target.value)
+            }
+          >
+            {recurrenceOptions?.map((recurrence, index) => (
+              <MenuItem key={index} value={recurrence}>
+                {recurrence}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            fullWidth
+            select
+            name="category"
+            label="Category"
+            margin="normal"
+            defaultValue="Any"
+            onChange={(event) =>
+              handleInputChange(event.target.name, event.target.value)
+            }
+          >
+            {categoryOptions?.map((category, index) => (
+              <MenuItem key={index} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            fullWidth
+            select
+            name="timeLimit"
+            label="Time Limit"
+            margin="normal"
+            defaultValue="Unlimited"
+            onChange={(event) =>
+              handleInputChange(event.target.name, event.target.value)
+            }
+          >
+            {["Unlimited", 5, 10, 15, 20, 25, 30, 35, 40, 45].map(
+              (time, index) => (
+                <MenuItem key={index} value={time}>
+                  {time}
+                </MenuItem>
+              ),
+            )}
+          </TextField>
+
+          <Collapse in={open}>
+            <Alert
+              variant="outlined"
+              severity="warning"
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+            >
+              <AlertTitle>Warning</AlertTitle>
+              No matching chores found. Change your recurrence/category/time
+              limit and try again.
+            </Alert>
+          </Collapse>
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ marginTop: 2 }}
+          >
+            Start Mission!
+          </Button>
+        </form>
+
+        {/* If you want a "Back" button */}
+        <Button
+          variant="text"
+          color="secondary"
+          onClick={() => window.history.back()}
+          sx={{ marginTop: 2 }}
+        >
+          Back
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default MissionForm;
--- src/Components/MissionPage/MissionView.tsx
+++ src/Components/MissionPage/MissionView.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import { useState } from "react";
 import ActiveMission from "~/Components/MissionPage/ActiveMission/ActiveMission";
-import MissionFormv2 from "~/Components/MissionPage/MissionForm/MissionFormv2";
+import MissionForm from "~/Components/MissionPage/MissionForm/MissionForm";
 import MissionChoreResponse from "~/types/Response/MissionChoreResponse";
 import UserData from "~/types/Response/UserData";
 
@@ -19,7 +19,7 @@ const MissionView = ({ userData }: MissionProps): ReactElement<string> => {
       {startMission ? (
         <ActiveMission missionChores={missionChores} />
       ) : (
-        <MissionFormv2
+        <MissionForm
           setStartMission={setStartMission}
           setMissionChores={setMissionChores}
           userData={userData}
