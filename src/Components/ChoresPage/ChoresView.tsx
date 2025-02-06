347d0b96887f057d6b1e10e47d7c61139809332a
Amber <110812972+Msambere@users.noreply.github.com>
2025-02-06 17:13:31 -0600

    Create sorting buttons

--- src/Components/ChoresPage/ChoresView.tsx
+++ src/Components/ChoresPage/ChoresView.tsx
@@ -1,7 +1,11 @@
 import UserData from "~/types/Response/UserData";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import ChoreList from "~/Components/ChoresPage/ChoreList";
+import SortingButtons from "~/Components/SharedComponents/SortingButtons";
+import { extractUserRecurrences } from "~/Helper Functions/extractUserRecurrences";
+import { extractUserCategories } from "~/Helper Functions/extractUserCategories";
+import sortData from "~/Helper Functions/sortData";
 
 interface ChoresProps {
   userData: UserData;
@@ -15,7 +19,35 @@ const ChoresView = ({ userData, setUserData }: ChoresProps) => {
       navigate("/");
     }
   }, []);
-  return <ChoreList userData={userData} setUserData={setUserData} />;
+
+  const recurrenceList: string[] = extractUserRecurrences(
+    userData.chores ?? [],
+  );
+  const categoryList: string[] = extractUserCategories(userData.chores ?? []);
+
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortValue, setSortValue] = useState<string>("title");
+
+  return (
+    <>
+      <SortingButtons
+        recurrenceList={recurrenceList}
+        categoryList={categoryList}
+        setSortOrder={setSortOrder}
+        setSortValue={setSortValue}
+        sortValue={sortValue}
+        sortOrder={sortOrder}
+      />
+      <ChoreList
+        userData={userData}
+        chores={sortData(userData.chores, sortValue, sortOrder)} //use sorted function on this sortData( UserData.chores, sortOptions, sortValue, sortOrder)
+        setUserData={setUserData}
+        recurrenceList={recurrenceList}
+        categoryList={categoryList}
+      />
+      ;
+    </>
+  );
 };
 
 export default ChoresView;
--- src/Components/SharedComponents/SortingButtons.tsx
+++ src/Components/SharedComponents/SortingButtons.tsx
@@ -1,10 +1,8 @@
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import React, { Dispatch, SetStateAction } from "react";
-import {OutlinedInput} from "@mui/material";
-
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -12,85 +10,62 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
+      minWidth: 125,
     },
   },
 };
-
-function getStyles(name: string, personName: readonly string[], theme: Theme) {
-  return {
-    fontWeight: personName.includes(name)
-      ? theme.typography.fontWeightMedium
-      : theme.typography.fontWeightRegular,
-  };
-}
-
 
 interface Props {
   recurrenceList: string[];
   categoryList: string[];
   setSortOrder: Dispatch<SetStateAction<string>>;
   setSortValue: Dispatch<SetStateAction<string>>;
-  sortValue: string[];
+  sortValue: string;
   sortOrder: string;
 }
 
-const SortingButtons = ({recurrenceList, categoryList, setSortOrder, setSortValue, sortValue, sortOrder}: Props) => {
-
-  const recurrenceOptions = recurrenceList.map((recurrence:string) => {
-    return (
-      <option key={recurrence} value={recurrence}>{recurrence}</option>
-    );
-  });
-
-  const categoryOptions = categoryList.map((category:string) => {
-    return (
-      <option key={category} value={category}>{category}</option>
-    );
-  });
-
+const SortingButtons = ({
+  setSortOrder,
+  setSortValue,
+  sortValue,
+  sortOrder,
+}: Props) => {
   const handleSortValueChange = (event) => {
     setSortValue(event.target.value);
+    console.log(sortValue);
   };
-
-  // const handleChange = (event: SelectChangeEvent<typeof personName>) => {
-  //   const {
-  //     target: { value },
-  //   } = event;
-  //   setPersonName(
-  //     // On autofill we get a stringified value.
-  //     typeof value === 'string' ? value.split(',') : value,
-  //   );
-  // };
 
   const handleSortOrderChange = (event) => {
     setSortOrder(event.target.value);
+    console.log(sortOrder);
   };
+  const sortOptions = [
+    "title",
+    "recurrence",
+    "category",
+    "duration",
+    "difficulty",
+  ];
 
   return (
     <div>
-      <FormControl sx={{ m: 1, minWidth: 80 }}>
+      <FormControl sx={{ m: 1, minWidth: 125 }}>
         <InputLabel htmlFor="sort-value-options">Sort by: </InputLabel>
         <Select
-          multiple
-          value={sortValue}
-          onChange={handleSortValueChange}
-          defaultValue=""
           id="sort-value-options"
           label="Sort by"
-          input={<OutlinedInput label="Sort by" />}
+          value={sortValue}
+          onChange={handleSortValueChange}
+          MenuProps={MenuProps}
         >
-          <option aria-label="None" value="" />
-          <option value="alphabetically">Alphabetically</option>
-          <optgroup label="Recurrence">
-            {recurrenceOptions}
-          </optgroup>
-          <optgroup label="Category">
-            {categoryOptions}
-          </optgroup>
+          {sortOptions.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option.charAt(0).toUpperCase() + option.slice(1)}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
-      <FormControl sx={{ m: 1, minWidth: 100 }}>
+      <FormControl sx={{ m: 1, minWidth: 125 }}>
         <InputLabel id="sort-order-label">Sort Order:</InputLabel>
         <Select
           labelId="sort-order-label"
@@ -106,6 +81,6 @@ const SortingButtons = ({recurrenceList, categoryList, setSortOrder, setSortValu
       </FormControl>
     </div>
   );
-}
+};
 
 export default SortingButtons;
